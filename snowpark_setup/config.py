8b1537e1079db276eedb818affc70db1ee2f5c80
bharath-reddy-t <106577810+bharath-reddy-t@users.noreply.github.com>
2023-05-08 10:00:41 +0530

    initial commit

---
+++ snowpark_setup/config.py
@@ -0,0 +1,10 @@
+SNOWFLAKE_CONN_PROFILE = {
+    "User": "<snowflake username>",
+    "password": "<snowflake password>",
+    "account": "<snowflake account identifier>",
+    "database": "<snowflake database>",
+    "schema": "<snowflake schema>",
+    "role": "<snowflake role>",
+    "warehouse": "<snowflake warehouse>",
+}
+
